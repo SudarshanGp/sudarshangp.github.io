81d129af1edc9253efdb5fc68dc5df02f345d6de
SudarshanGp <Sudarshan@Sudarshans-MacBook-Air.local>
2015-07-12 18:07:36 -0500

    test

---
+++ test.js
@@ -0,0 +1,15 @@
+  // for (var i=0; i < slideqty; i++) {
+  //   var insertText = '<li data-target="#featured" data-slide-to="' + i + '"';
+  //   if (i === randSlide) {
+  //     insertText += ' class="active" ';
+  //   }
+  //   insertText += '></li>';
+  //   $('#featured ol').append(insertText);
+  // }
+
+  // $('.carousel').carousel({
+  //   pause: false
+  // });
+  $('.carousel').carousel({
+  interval: true
+});
